5a05803b7ed40060f6d762a4dbe1c1e16edf6fc8
Dima Antonenko <daemondw@gmail.com>
2020-10-13 13:17:47 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      theme: ThemeData(
+        primarySwatch: Colors.amber
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Testing')),
+      body: Container(
+        child: Center(
+          child: Text('Here will be something'),
+        ),
+      ),
+    );
+  }
+}
+
