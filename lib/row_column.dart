5782e4769bb55880caf02941d27f135a4542fd54
Dima Antonenko <daemondw@gmail.com>
2020-10-13 13:55:51 +0200

    testing

---
+++ lib/row_column.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MyRowColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('theory buld layouts'),
+      ),
+      body: Container(
+        color: Colors.grey,
+        child: Row(
+          children: <Widget>[
+            ColorBox(),
+            ColorBox(),
+            ColorBox(),
+            BiggerColorBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ColorBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80,
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.redAccent[400],
+        border: Border.all(),
+      ),
+    );
+  }
+}
+
+class BiggerColorBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.redAccent[700],
+        border: Border.all(),
+      ),
+    );
+  }
+}
